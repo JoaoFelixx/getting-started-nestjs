1b6d7c686ab9aa1810f193f9d74d319309163726
Felix developer <felixxdeveloper@gmail.com>
2022-08-06 21:45:43 -0300

    ADD/REF - Adding new ednpoints and refactored others endpoints

--- src/useCases/users/users.controller.ts
+++ src/useCases/users/users.controller.ts
@@ -1,13 +1,108 @@
-import { Controller, Get } from '@nestjs/common';
-import { UsersService } from './users.service';
 import { User } from '../../entities';
+import { Response } from 'express';
+import { UsersService } from './users.service';
+import { randomUUID as uuid } from 'crypto';
+import {
+  Get,
+  Res,
+  Put,
+  Post,
+  Body,
+  Param,
+  Delete,
+  HttpCode,
+  Controller,
+} from '@nestjs/common';
 
-@Controller()
+@Controller('users')
 export class UsersController {
-  constructor(private readonly appService: UsersService) {}
+  private readonly regexEmail: RegExp;
 
-  @Get('users')
-  public async index(): Promise<User[]> {
+  constructor(private readonly appService: UsersService) {
+    this.regexEmail = new RegExp(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/);
+  }
+
+  @Get()
+  @HttpCode(200)
+  public async getUsers() {
     return await this.appService.findAll();
+  }
+
+  @Get(':id')
+  public async getUser(@Param() params, @Res() response: Response) {
+    const { id } = params;
+
+    const result = await this.appService.findOne(id);
+
+    if (!result) {
+      return response.sendStatus(204);
+    }
+
+    response.json(result);
+  }
+
+  @Post()
+  public async createUser(
+    @Body() body: Omit<User, '_id'>,
+    @Res() response: Response,
+  ) {
+    try {
+      const user: User = {
+        ...body,
+        _id: uuid(),
+      };
+
+      if (!this.regexEmail.test(user.email)) {
+        return response.status(400).json('Email is invalid');
+      }
+
+      const result = await this.appService.create(user);
+
+      response.status(201).json(result);
+    } catch (error) {
+      response.status(400).json('Error creating user');
+    }
+  }
+
+  @Delete(':id')
+  public async deleteUser(@Param() params, @Res() response: Response) {
+    try {
+      const { id } = params;
+
+      const result = await this.appService.remove(id);
+
+      if (result instanceof Error) {
+        return response.status(400).json(result.message);
+      }
+
+      response.status(204).json('User deleted');
+    } catch (error) {
+      response.status(400).json('Error deleting user');
+    }
+  }
+
+  @Put(':id')
+  public async updateUser(
+    @Param() params,
+    @Body() body: User,
+    @Res() response: Response,
+  ) {
+    try {
+      const { id } = params;
+      const user: User = {
+        ...body,
+        _id: id,
+      };
+
+      const result = await this.appService.update(user);
+
+      if (result instanceof Error) {
+        return response.status(400).json(result.message);
+      }
+
+      response.status(202).json(result);
+    } catch (error) {
+      return response.status(400).json('Error updating user');
+    }
   }
 }
--- src/useCases/users/users.service.ts
+++ src/useCases/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Users } from '../../entities';
+import { Users, User } from '../../entities';
 import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -15,10 +15,44 @@ export class UsersService {
   }
 
   async findOne(id: string): Promise<Users> {
-    return await this.usersRepository.findOne({ where: { _id: id } });
+    return await this.usersRepository.findOneBy({ _id: id });
   }
 
-  async remove(id: string): Promise<void> {
+  async remove(id: string): Promise<void | Error> {
+    const result = await this.usersRepository.findOneBy({ _id: id });
+
+    if (!result) {
+      return new Error('User does not registered');
+    }
+
     await this.usersRepository.delete(id);
+  }
+
+  async create(user: User): Promise<Users> {
+    const createdUser = this.usersRepository.create(user);
+
+    const result = await this.usersRepository.save(createdUser);
+
+    return result;
+  }
+
+  async update(user: User): Promise<Users | Error> {
+    try {
+      const userExits = await this.usersRepository.findOneBy({ _id: user._id });
+
+      if (!userExits) {
+        return new Error('User does not exists');
+      }
+
+      Object.assign(userExits, user);
+
+      const updateUser = await this.usersRepository.create(userExits);
+
+      await this.usersRepository.save(updateUser);
+
+      return updateUser;
+    } catch (error) {
+      return new Error('Error updating user');
+    }
   }
 }
